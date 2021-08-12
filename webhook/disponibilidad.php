9c9906bca470b2e2c56195f5ed6ada9d6b0487eb
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-08-12 15:32:56 -0500

    add function nombreMes

--- webhook/disponibilidad.php
+++ webhook/disponibilidad.php
@@ -44,47 +44,19 @@ if (intent_recibido("disponibilidad")) {
             }
       }
 
-      if(!$existe){
+      if (!$existe) {
             $mensaje = "No se encuentran registros";
       }
 
-      enviar_texto("Sillas disponibles en el mes($mes) año($ano)\n para $origen > $destino son:\n ____________________________________\n $mensaje  ____________________________________\n Para otra consulta o chatear con un asesor escribe MENU \n ____________________________________");
+      $mes_nombre = asignarNombreMes($mes);
+      enviar_texto("Sillas disponibles en el mes($mes_nombre) año($ano)\n para $origen > $destino son:\n ____________________________________\n $mensaje  ____________________________________\n Para otra consulta o chatear con un asesor escribe MENU \n ____________________________________");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 //***************************
 //**** FUNCIONES ************
 //***************************
-
-
 
 
 function formatDate($date)
@@ -107,12 +79,49 @@ function formatDate($date)
 }
 
 
-
-
-
-
-
-
+function asignarNombreMes($numero)
+{
+      $mes = "";
+      switch ($numero) {
+            case 1:
+                  $mes = "Enero";
+                  break;
+            case 2:
+                  $mes = "Febrero";
+                  break;
+            case 3:
+                  $mes = "Marzo";
+                  break;
+            case 4:
+                  $mes = "Abril";
+                  break;
+            case 5:
+                  $mes = "Mayo";
+                  break;
+            case 6:
+                  $mes = "Junio";
+                  break;
+            case 7:
+                  $mes = "Julio";
+                  break;
+            case 8:
+                  $mes = "Agosto";
+                  break;
+            case 9:
+                  $mes = "Septiembre";
+                  break;
+            case 10:
+                  $mes = "Octubre";
+                  break;
+            case 11:
+                  $mes = "Noviembre";
+                  break;
+            case 12:
+                  $mes = "Diciembre";
+                  break;
+      }
+      return $mes;
+}
 
 
 
