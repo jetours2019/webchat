8db6a000023853b2426990f9ae903b2a1771c9ac
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-09-25 10:20:21 -0500

    añadir segundo canal en conteo de clientes atendiendo

--- crons/update_from_respond.php
+++ crons/update_from_respond.php
@@ -10,28 +10,40 @@ while ($registro = mysqli_fetch_array($consulta)) {
 }
 
 $url = "https://app.respond.io/api/v1/contact/by_custom_field?name=asignado&value=";
-$accesstoken = "1bc636f151a3909b02451ebd431f0ca424bc744f9ef03090892e4acd100b74f0f9827eef184095d00cec29a56ba966ac5340e3dc217ea02152111eef5a18c50b";
+$accesstoken1 = "1bc636f151a3909b02451ebd431f0ca424bc744f9ef03090892e4acd100b74f0f9827eef184095d00cec29a56ba966ac5340e3dc217ea02152111eef5a18c50b";
+$accesstoken2 = "ef59b580ab4c438e0be3e39d8d095152dae9da799d4a858a4e9388db25decf77a5772b615c2d0b4dc4353629173d097ed3e4e217781642a102c1118126eab51a";
 
 foreach ($tags as $id=>$tag) {
 
     $ch = curl_init($url . $tag);
     $headr = array();
     $headr[] = 'Content-type: application/json';
-    $headr[] = 'Authorization: Bearer ' . $accesstoken;
+    $headr[] = 'Authorization: Bearer ' . $accesstoken1;
 
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $response = json_decode(curl_exec($ch));
-    $clients = countContacts($response);
+    $clients = countContacts($response, $accesstoken1);
+
+    $ch2 = curl_init($url . $tag);
+    $headr2 = array();
+    $headr2[] = 'Content-type: application/json';
+    $headr2[] = 'Authorization: Bearer ' . $accesstoken2;
+
+    curl_setopt($ch2, CURLOPT_HTTPHEADER, $headr2);
+    curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+
+    $response = json_decode(curl_exec($ch));
+    $clients += countContacts($response, $accesstoken2);
+
     $sql = "UPDATE usuarios SET clients=$clients WHERE id=$id";
     $act = $conexion->query($sql);
 }
 
 
-function countContacts($response)
+function countContacts($response, $accesstoken)
 {
-    global $accesstoken;
 
     if (!$response->links->next) {
         return count($response->data);
@@ -47,5 +59,5 @@ function countContacts($response)
 
     $newResponse = json_decode(curl_exec($ch));
 
-    return count($response->data) + countContacts($newResponse);
+    return count($response->data) + countContacts($newResponse, $accesstoken);
 }
