1f146459d9a729024c7f95a8affae4abb542d431
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-10-02 08:50:42 -0500

    update token

--- crons/update_from_respond.php
+++ crons/update_from_respond.php
@@ -10,32 +10,32 @@ while ($registro = mysqli_fetch_array($consulta)) {
 }
 
 $url = "https://app.respond.io/api/v1/contact/by_custom_field?name=asignado&value=";
-$accesstoken1 = "1bc636f151a3909b02451ebd431f0ca424bc744f9ef03090892e4acd100b74f0f9827eef184095d00cec29a56ba966ac5340e3dc217ea02152111eef5a18c50b";
-$accesstoken2 = "ef59b580ab4c438e0be3e39d8d095152dae9da799d4a858a4e9388db25decf77a5772b615c2d0b4dc4353629173d097ed3e4e217781642a102c1118126eab51a";
+$accesstoken_fb = "1bc636f151a3909b02451ebd431f0ca424bc744f9ef03090892e4acd100b74f0f9827eef184095d00cec29a56ba966ac5340e3dc217ea02152111eef5a18c50b";
+$accesstoken_wp = "6c661ebf6c21f020c409880bdb95eef4769b7879184d0428c36e003d1092a2160e6dd564ca6103b398939c3963c1a30de07af5139ebb589204e32f9be0c62362";
 
 foreach ($tags as $id=>$tag) {
 
     $ch = curl_init($url . $tag);
     $headr = array();
     $headr[] = 'Content-type: application/json';
-    $headr[] = 'Authorization: Bearer ' . $accesstoken1;
+    $headr[] = 'Authorization: Bearer ' . $accesstoken_fb;
 
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $response = json_decode(curl_exec($ch));
-    $clients = countContacts($response, $accesstoken1);
+    $clients = countContacts($response, $accesstoken_fb);
 
     $ch2 = curl_init($url . $tag);
     $headr2 = array();
     $headr2[] = 'Content-type: application/json';
-    $headr2[] = 'Authorization: Bearer ' . $accesstoken2;
+    $headr2[] = 'Authorization: Bearer ' . $accesstoken_wp;
 
     curl_setopt($ch2, CURLOPT_HTTPHEADER, $headr2);
     curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 
     $response = json_decode(curl_exec($ch2));
-    $clients += countContacts($response, $accesstoken2);
+    $clients += countContacts($response, $accesstoken_wp);
 
     $sql = "UPDATE usuarios SET clients=$clients WHERE id=$id";
     $act = $conexion->query($sql);
